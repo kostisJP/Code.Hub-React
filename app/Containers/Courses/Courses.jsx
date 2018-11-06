48d5591d1a94648727d9823bdf8c5af2eb58456c
Konstantinos Kefalas <kostis@Konstantinoss-MacBook-Pro-2.local>
2018-11-06 22:59:30 +0200

    courses page

--- app/Containers/Courses/Courses.jsx
+++ app/Containers/Courses/Courses.jsx
@@ -1,11 +1,26 @@
 import React  from "react";
 import "./Courses.css";
+import { getAllCourses } from "../../api/calls";
+import CourseCard from "../../components/CourseCard/CourseCard";
 
 class Courses extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {      
+      courses: []      
+    };    
+  }
+
+  componentDidMount(){
+    getAllCourses().then(courses => this.setState({courses}));
+  }
+
   render() {
     return (
       <div>
-        
+        <h2>Courses <span style={{color: "grey", fontSize: 16}}>(all)</span></h2>        
+        {this.state.courses.map(course=>
+          <CourseCard key={course.id} {...course} />)}
       </div>
     );
   }
